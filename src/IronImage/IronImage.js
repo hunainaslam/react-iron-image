71b038c05c5668129628a6e7c6114b2a8432267e
Bojan Gvozderac <bojan.gvozderac@jsguru.io>
2017-08-11 00:49:52 +0200

    React IronImage

--- src/IronImage/IronImage.js
+++ src/IronImage/IronImage.js
@@ -3,23 +3,30 @@ import './IronImage.css';
 
 class IronImage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.ironImageHd = null;
+  }
+
   componentDidMount() {
 
-    const ironImageHd = document.querySelector('.iron-image-loaded');
+    // const ironImageHd = document.querySelector('.iron-image-loaded');
+    
     const hdLoaderImg = new Image();
 
     hdLoaderImg.src = this.props.srcLoaded;
 
     hdLoaderImg.onload = () => {
-      ironImageHd.setAttribute('style', `background-image: url('${this.props.srcLoaded}')`);
-      ironImageHd.classList.add('iron-image-fade-in');
+      this.ironImageHd.setAttribute('style', `background-image: url('${this.props.srcLoaded}')`);
+      this.ironImageHd.classList.add('iron-image-fade-in');
     }
-  }
+
+  };
 
   render() {
     return (
       <div className="iron-image-container">
-        <div className="iron-image-loaded"></div>
+        <div className="iron-image-loaded" ref={imageLoadedElem => this.ironImageHd = imageLoadedElem}></div>
         <div className="iron-image-preload" style={{ backgroundImage: `url('${this.props.srcPreload}')` }}></div>
       </div>
     )
